43969a1f83be834e749675bc2ca8d212cf01e187
Jacob Graham <147766742+JacobTheCoder19@users.noreply.github.com>
2025-09-07 03:59:37 +0000

    Added more things

--- sketch.js
+++ sketch.js
@@ -30,21 +30,34 @@ function draw() {
   text("🐞", mouseX, mouseY) //ladybug
 }
 */
-
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
-  //Place code here
-  
-  
-  
-  
-  
-  /* places the x a y position of the mouse
-  on the canvas as a coordinate pair x, y */
-  fill(0)
-  text(`${mouseX}, ${mouseY}`, 20, 20);
+  background('navy'); //navy background
+
+  //moon
+  fill(255);
+  stroke(0);
+  circle(350, 50, 100);
+
+  //overlapping navy circle for crescent moon
+  stroke("navy");   
+  fill("navy");
+  circle(320,50,100);
+
+  //big gray mountains
+  stroke(0);
+  fill(80);
+  triangle(-40,300,75,100, 250,300);
+  triangle(100,300,300,100, 500,300);
+
+  //grass
+  fill('rgb(50,76,50)');
+  rect(0,300, 400, 100);
+
+  //displays the x and y position of the mouse on the canvas
+  fill(255) //white text
+  text(`mouseX: ${mouseX}, mouseY: ${mouseY}`, 20, 20);  
 }

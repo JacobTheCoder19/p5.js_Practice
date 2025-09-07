37197d31c6b3ced4509df5e71bcdb6dc2cc41a74
Jacob Graham <147766742+JacobTheCoder19@users.noreply.github.com>
2025-09-07 04:07:03 +0000

    added

--- sketch.js
+++ sketch.js
@@ -1,35 +1,4 @@
-/* ||| PRACTICE BASICS
-function setup() {
-  createCanvas(600, 400);
-}
 
-function draw() {
-  // Sky blue background
-  background(135, 206, 235);
-  // Sun in top right
-  fill("yellow");
-
-  stroke("orange"); // Orange outline
-
-  strokeWeight(20); //large outling
-
-  circle(550, 50, 100);
-  //grass on bottom half
-
-  stroke(0); //black outline
-
-  strokeWeight(1); //outline thickness
-
-  fill("green");
-
-  rect(0, 200, 600, 200);
-
-  //emojis
-  textSize(75);
-  text("🌸", 100, 250) //flower
-  text("🐞", mouseX, mouseY) //ladybug
-}
-*/
 function setup() {
   createCanvas(400, 400);
 }

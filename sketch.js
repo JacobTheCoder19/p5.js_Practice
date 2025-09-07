595894ad906d12ae4ffcfa686c2de4d723dae8ae
Jacob Graham <147766742+JacobTheCoder19@users.noreply.github.com>
2025-09-07 06:35:28 +0000

    Flashlight Mechanic (working I think!)

--- sketch.js
+++ sketch.js
@@ -1,16 +1,30 @@
+let outerLayer;
 function setup() {
-  createCanvas(1000, 1000);
-  // Hide the cursor.
+  // Creates the base canvas
+  createCanvas(400, 400);
+  // Creates the canvas that will cover the bottom one
+  outerLayer = createGraphics(width,height);
+  // This just removes the cursor so it looks like a flashlight but idk if it will screw with the ability to click the shapes later on in development
   noCursor();
-  let c = color(0,0,255);
-
 }
 
 function draw() {
-  background('green');
-  c.setAlpha(128);
-  fill(c);
-  circle(mouseX, mouseY, 100);
+  background(color(0,70,70));
+  fill(255,0,0);
+  // I created a shape and I tried it with text in case we want to use emojis or shapes
+  square(100,150,60);
+  textSize(100);
+  text('🍄',200,100)
+  
+  // Creates a black square that covers the whole canvas. The last paramater will need to be updated to whatever size we make our canvas, I just did 400 for testing! 
+  outerLayer.fill(0,0,0);
+  outerLayer.square(0,0,400)
+  
+  // Makes a circle that erases the square. The trick to making it look like the flashlight is I used the mouseX and mouseY variables so it just makes the circle move with the mouse! I just did circle but it can be any shape prety much as long as it's in between the erase() and noErase()! -- I did try other shapes but I think circle fits best as a flashlight!
+  outerLayer.erase();
+  outerLayer.circle(mouseX,mouseY,90);
+  outerLayer.noErase();
 
-  describe('A white circle on a gray background. The circle follows the mouse as it moves. The cursor is hidden.');
+  // Displays the outer layer, DO NOT REMOVE OR THE SECOND LAYER WILL NOT SHOW!!!!
+  image(outerLayer,0,0);
 }

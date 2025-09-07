558ceb6e300236b5dfbd4576bccf5dc1e100041f
Jacob Graham <147766742+JacobTheCoder19@users.noreply.github.com>
2025-09-07 05:02:09 +0000

    Trying to add and change the transparency

--- sketch.js
+++ sketch.js
@@ -2,10 +2,14 @@ function setup() {
   createCanvas(1000, 1000);
   // Hide the cursor.
   noCursor();
+  let c = color(0,0,255);
+
 }
 
 function draw() {
   background('green');
+  c.setAlpha(128);
+  fill(c);
   circle(mouseX, mouseY, 10);
 
   describe('A white circle on a gray background. The circle follows the mouse as it moves. The cursor is hidden.');
